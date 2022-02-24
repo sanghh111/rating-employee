099787760ed59ac643fd0d0ddfe990e7fb12297c
hieunbk <nguyenhieu2542000@gmail.com>
2022-02-24 14:30:57 +0700

    update urls

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
-from .views import *
 
+from django.urls import path, include
 
 urlpatterns = [
-    path('group-skill/', GroupSkillAPIView.as_view()),
-    path('group-skill/', GroupSkillAPIView.as_view())
-
+    path('groupskill/', include("group_skill.urls")),
+    path('project/', include("project.urls")),
+    path('projectuserwork/', include("project_user_work.urls")),
+    path('rating/', include("rating.urls")),
+    path('user/', include("user.urls")),
+    path('userskill/', include("userskill.urls")),
 ]

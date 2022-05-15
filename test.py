2bc024140940d411b348d37e712545cb31ef1d6d
sanghh111 <sanghh111@gmail.com>
2022-05-15 17:01:07 +0700

    add test file

---
+++ test.py
@@ -0,0 +1,25 @@
+import os
+
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rating_employee.settings')
+
+
+django.setup()
+
+from core.user.models import User
+from api.user.serializers import UserSerializer
+
+
+# dict_user = {
+#     'username' : 'sangh',
+#     'password' : '123', 
+#     # 'confirm_password' : '123',
+#     'first_name' : 'Nguyen'
+    
+# }
+user =  User.objects.get(username = 'admin')
+
+data =  UserSerializer(user)
+
+print(data.data)

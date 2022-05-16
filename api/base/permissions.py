9b904a4b39266d65fa973bd55cd7abdc951065dd
sanghh111 <sanghh111@gmail.com>
2022-05-16 14:50:59 +0700

    config permission flowwing role

--- api/base/permissions.py
+++ api/base/permissions.py
@@ -1,12 +1,23 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission,DjangoModelPermissions
 
-class IsManagerUser(BasePermission):
+class IsManagerUser(DjangoModelPermissions):
 
     """
-    allow access user Manager or higher
+    allow access user Manager or higher or role user have access 
     """
 
     def has_permission(self, request, view):
 
-        return bool(request.user.rank == "MANAGER" or request.user.is_supperuser)
-        # return super().has_permission(request, view)    
+        if getattr(view, '_ignore_model_permissions', False):
+            return True
+
+        if not request.user or (
+           not request.user.is_authenticated and self.authenticated_users_only):
+            return False 
+
+
+        queryset = self._queryset(view)
+        perms = self.get_required_permissions(request.method, queryset.model)
+        print('perms: ', perms)
+
+        return bool(request.user.has_perms(perms) or request.user.rank == 'MANAGER' or request.user.is_superuser )

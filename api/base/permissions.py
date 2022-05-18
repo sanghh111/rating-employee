f0a2e1c939a30ab11ac4e279388eb55801e4b937
sanghh111 <sanghh111@gmail.com>
2022-05-18 17:28:57 +0700

    Rename IsactiveUser and config has_permission

--- api/base/permissions.py
+++ api/base/permissions.py
@@ -6,6 +6,16 @@ class IsManagerUser(DjangoModelPermissions):
     allow access user Manager or higher or role user have access 
     """
 
+    perms_map = {
+        'GET': ['list_%(model_name)s'],
+        'OPTIONS': [],
+        'HEAD': [],
+        'POST': ['add_%(model_name)s'],
+        'PUT': ['change_%(model_name)s'],
+        'PATCH': ['change_%(model_name)s'],
+        'DELETE': ['delete_%(model_name)s'],
+    }
+
     def has_permission(self, request, view):
 
         if getattr(view, '_ignore_model_permissions', False):
@@ -18,6 +28,5 @@ class IsManagerUser(DjangoModelPermissions):
 
         queryset = self._queryset(view)
         perms = self.get_required_permissions(request.method, queryset.model)
-        print('perms: ', perms)
 
-        return bool(request.user.has_perms(perms) or request.user.rank == 'MANAGER' or request.user.is_superuser )
+        return bool(request.user.role_is_active(perms) or request.user.is_superuser )

8e084920c5082518d3e12e8aa36f852961a9b581
sanghh111 <sanghh111@gmail.com>
2022-05-19 10:46:53 +0700

    build URL UserPermission

--- api/permission/user/urls.py
+++ api/permission/user/urls.py
@@ -3,5 +3,8 @@ from .views import APIUserPermission
 urlpatterns = [
     path('create',APIUserPermission.as_view({
             'post':'create'
+    })),
+    path('delete',APIUserPermission.as_view({
+            'delete' : 'delete'
     }))
 ]

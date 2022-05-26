86fa7fd5e9aa568dbbab116edad1b8d1e878bfca
sanghh111 <sanghh111@gmail.com>
2022-05-26 13:49:12 +0700

    ADD swager rating

--- api/urls.py
+++ api/urls.py
@@ -5,11 +5,11 @@ urlpatterns = [
     path('group-skill/', include("api.group_skill.urls")),
     path('project', include("api.project.urls")),
     path('project-user-work/', include("api.project_user_work.urls")),
+    path('rating', include("api.rating.urls")),
     # path('user/', include("api.user.urls")),
-    # path('rating', include("api.rating.urls")),
-    # path('user-skill/', include("api.user_skill.urls")),
+    path('user-skill/', include("api.user_skill.urls")),
     # path('role/', include("api.role.urls")),
     # path('user-role/', include("api.user_role.urls")),
     # path('user-role-permission/', include("api.user_role_permission.urls")),
     # path('permission/', include("api.permission.urls")),   
-]
+]   

eaa28430dcf5421819ad6530c609bca93e89d93c
sanghh111 <sanghh111@gmail.com>
2022-05-28 23:43:20 +0700

    delete body

--- api/rating/manager/view.py
+++ api/rating/manager/view.py
@@ -46,43 +46,39 @@ class APIRating(BaseAPIView):
     )
     def create(self,request,*args,**kwargs):
         
-        if not  request.body :
-            return Response(data  = "NOT BODY",
-                            status = status.HTTP_400_BAD_REQUEST)
-        else:
-            data  = orjson.loads(request.body)
+        data  = orjson.loads(request.body)
 
-            data_dict = {
-                'user_id_rated': request.data.get('user_id_rated',None),
-                'session_rating': request.data.get('session_rating',None),
-                'description' : request.data.get('description',None),
-                'score' : None,
-                'create_at' : datetime.now(),
-                'create_by' : request.user.username
-            }
-                            
-            if data_dict['user_id_rated']:
-                try:
-                    data_dict['user_id_rated']= User.objects.get(id = data_dict['user_id_rated'])
-                    if not data_dict['score']:
-                        return Response(
-                            data =  "MISSING DATA SCORE",
-                            status=  status.HTTP_400_BAD_REQUEST
-                        )
-                    rating = Rating.objects.create(**data_dict)
-                    serializer_data = RatingSerializer(rating).data
+        data_dict = {
+            'user_id_rated': request.data.get('user_id_rated',None),
+            'session_rating': request.data.get('session_rating',None),
+            'description' : request.data.get('description',None),
+            'score' : None,
+            'create_at' : datetime.now(),
+            'create_by' : request.user.username
+        }
+                        
+        if data_dict['user_id_rated']:
+            try:
+                data_dict['user_id_rated']= User.objects.get(id = data_dict['user_id_rated'])
+                if not data_dict['score']:
                     return Response(
-                        data =  serializer_data,
-                        status= status.HTTP_201_CREATED
+                        data =  "MISSING DATA SCORE",
+                        status=  status.HTTP_400_BAD_REQUEST
                     )
-                except User.DoesNotExist: 
-                    return Response(data =  "NOT FOUND USER",
-                                    status= status.HTTP_404_NOT_FOUND)
-
-            else:
+                rating = Rating.objects.create(**data_dict)
+                serializer_data = RatingSerializer(rating).data
                 return Response(
-                            data =  "MISSING DATA SCORE",
-                            status=  status.HTTP_400_BAD_REQUEST
-                        )
+                    data =  serializer_data,
+                    status= status.HTTP_201_CREATED
+                )
+            except User.DoesNotExist: 
+                return Response(data =  "NOT FOUND USER",
+                                status= status.HTTP_404_NOT_FOUND)
+
+        else:
+            return Response(
+                        data =  "MISSING DATA SCORE",
+                        status=  status.HTTP_400_BAD_REQUEST
+                    )
 
         
